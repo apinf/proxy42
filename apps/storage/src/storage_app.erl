f97ccf14bca1e4dc6a9a9ef41aa1039eddd06bc0
Saurabh Kumar <saurabhgeek92@gmail.com>
2017-05-12 06:12:00 +0530

    Add a new storage OTP application.
    Currently it uses mnesia as a storage backend. mnesia can act as a
    distributed database, and the initialisation code reflects this.
    It checks the number of nodes, and based on result, it either sets up
    the database(if it is the first node), or it adds itself as a node.
    
    This is not heavily tested(or complete) at this point, and needs to be
    made production-ready.
    
    Notes:
    mnesia chooses CP in the CAP model, so sync can take time in case of netsplit.
    Merges may need to be manual.
    This is probably okay, as long as we just keep only config in mnesia.
    mnesia allows us to have proxy42 as a completely embedded program.
    
    But other options, like riak also need to be tested.

---
+++ apps/storage/src/storage_app.erl
@@ -0,0 +1,90 @@
+-module(storage_app).
+-behaviour(application).
+
+-export([start/2, init/0, stop/1]).
+-export([test/2]).
+
+%% announce
+-record(storage_announce, {sender, node}).
+%% ack announce
+-record(storage_ack_announce, {sender, node}).
+%% test_row
+-record(test, {key, value}).
+
+start(_StartType, _StartArgs) ->
+    register(?MODULE, Pid=spawn_link(?MODULE, init, [])),
+    {ok, Pid}.
+
+init() ->
+    %% TODO: Take in a list of distributed hosts.
+    Nodes = net_adm:world_list([net_adm:localhost()]),
+    erlang:display(Nodes),
+    NodeCount = length(Nodes),
+    case NodeCount > 1 of
+        true ->
+            init_storage(false),
+            announce_storage_to_nodes(Nodes);
+       false ->
+            init_storage(true)
+    end,
+    loop([]).
+
+init_storage(false) ->
+    mnesia:start();
+init_storage(true) ->
+    init_storage(false),
+    mnesia:change_table_copy_type(schema, node(), disc_copies),
+    create_tables(),
+    wait_for_tables().
+
+create_tables() ->
+    mnesia:create_table(domain_groups, [{attributes, [hostname
+                                                     ,frontend_prefix
+                                                     ,backend_prefix
+                                                     ,servers
+                                                     ,strategy
+                                                     ,additional_headers
+                                                     ,rate_limit]}
+                                       ,{type, bag}
+                                       ,{disc_copies, [node()]}]).
+
+wait_for_tables() ->
+    mnesia:wait_for_tables([domain_groups], 10000).
+
+announce_storage_to_nodes([Node|T]) when Node =:= node() ->
+    announce_storage_to_nodes(T);
+announce_storage_to_nodes([Node|T]) ->
+    Pid = rpc:call(Node, erlang, whereis, [storage]),
+    Pid ! #storage_announce{sender=self(), node=node()},
+    announce_storage_to_nodes(T);
+announce_storage_to_nodes([]) -> ok.
+
+loop(State) ->
+    receive
+        #storage_ack_announce{sender=_Sender, node=_Node} ->
+            loop(State);
+        #storage_announce{sender=Sender, node=Node} ->
+            copy_storage_to_node(Node),
+            Sender ! #storage_ack_announce{sender=self(), node=node()},
+            loop(State);
+        _M ->
+            loop(State)
+    end.
+
+copy_storage_to_node(Node) ->
+    _R1 = mnesia:change_config(extra_db_nodes, [Node]),
+    _R2 = mnesia:change_table_copy_type(schema, Node, disc_copies),
+    copy_tables(Node).
+
+copy_tables(Node) ->
+    _R1 = mnesia:add_table_copy(test, Node, disc_copies).
+
+test(K, V) ->
+    Row = #test{key=K, value=V},
+    F = fun() ->
+                mnesia:write(Row)
+        end,
+    mnesia:transaction(F).
+
+stop(_State) ->
+    ok.
