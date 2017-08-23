fcb07c5326d7057a8c7fbb939fbe1b41249b1a51
J Phani Mahesh <code@phanimahesh.me>
2017-08-23 20:29:11 +0530

    use proper module name for storage

--- apps/proxy42/src/proxy42_config.erl
+++ apps/proxy42/src/proxy42_config.erl
@@ -8,7 +8,7 @@ domain_config(Domain, Path) ->
     [<<"">>, Prefix, _] = re:split(Path, "/", [{return, binary},{parts, 3}]),
     % TODO: Fix this to consider incoming domain.
     %    This requires adding incoming domain to domain group record.
-    DG = proxy42_storage_app:find_domain_group(#domain_group{frontend_prefix = <<"/", Prefix/binary, "/">>, _ = '_'}),
+    DG = proxy42_storage:find_domain_group(#domain_group{frontend_prefix = <<"/", Prefix/binary, "/">>, _ = '_'}),
   % DomainGroup can be an arbitrary datastructure.
   % This gets passed to checkout_service,
   %   which returns a service, again an arbitrary data structure
